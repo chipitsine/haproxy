9efade58e66c9990aeadc6c8d60b4c892017b8db
Ilya Shipitsin <chipitsine@gmail.com>
2025-05-01 22:07:33 +0200

    test

--- .github/matrix.py
+++ .github/matrix.py
@@ -180,7 +180,7 @@ def main(ref_name):
                 "CC": CC,
                 "FLAGS": [
                     "USE_OBSOLETE_LINKER=1",
-                    'ARCH_FLAGS="-g -fsanitize=address"',
+                    'ARCH_FLAGS="-g -fsanitize=thread"',
                     'OPT_CFLAGS="-O1"',
                     "USE_ZLIB=1",
                     "USE_OT=1",
